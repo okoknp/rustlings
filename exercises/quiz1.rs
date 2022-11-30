0c28da386b6c09f1424ab88318d933b7afa14258
Pavan Nambi <92514129+okoknp@users.noreply.github.com>
2022-11-30 11:49:42 +0000

    fucntions nd if

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(price:i128)->i128 {
+if price>40{
+    price
+}
+else{
+    price*2
+}
+
+}
 
 // Don't modify this function!
 #[test]
